0cb123249bc87dafab734498510b3a51dafc8b90
joshuayarjah <joshuayarjah99@gmail.com>
2024-03-10 04:56:38 +0000

    --

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path ('', views.index, name='index'),
     path ('debtor-detail', views.debtor_detail, name='debtor_detail'),
+    path ('debtor-list', views.debtor_list, name='debtor_list'),
     path ('payment-form', views.payment_form, name='payment_form'),
     path ('transaction-detail', views.transaction_detail, name='transaction_details'),
     path ('dashboard', views.dashboard, name='dashboard'),
