57d98bb0e523117884689d72598298e131675704
joshuayarjah <joshuayarjah99@gmail.com>
2024-03-27 22:54:32 +0000

    --

--- debt_tracker/forms.py
+++ debt_tracker/forms.py
@@ -1,30 +1,21 @@
 from django import forms
-from .models import Debtor, Transaction
-# from django_select2.forms import ModelSelect2Widget
+from .models import Transaction, Debtor
 
 class TransactionForm(forms.ModelForm):
-    # generate_transaction_id = forms.BooleanField(widget=forms.HiddenInput(), required=False)
-
     class Meta:
         model = Transaction
-        fields = '__all__'  # Include all fields from the Transaction model
-
+        fields = '__all__'  # Include all fields from the Transaction model except 'unique_id'
+        exclude = ['unique_id']  # Exclude 'unique_id' field from the form
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
         }
-
-
+        
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs.update({'class': 'form-control'})
 
-        # Populate the debtor field with all debtors
-        self.fields['debtor'].queryset = Debtor.objects.all()
 
-        # Check if the form is submitted with the generate_transaction_id field set to True
-        if self.data.get('generate_transaction_id') == 'True':
-            self.instance.generate_transaction_id()  # Generate transaction ID
 
 
 class DebtorForm(forms.ModelForm):
